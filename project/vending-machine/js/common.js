3fb8f920a189b485c736a94ad3d2fd5ae73dee69
seungmi <seungmimi10@gmail.com>
2023-08-01 17:40:41 +0900

    🎉[add folder]velog

--- project/vending-machine/js/common.js
+++ project/vending-machine/js/common.js
@@ -1,4 +1,16 @@
 /*
+1. 입금을 하면 tost에 클레스 active 추가 -> 3초뒤에 클레스 삭제
+2. 반환을 하면 tost에 클레스 active 추가 -> 3초뒤에 클레스 삭제
+*/
+let tostMessage = document.getElementById('tost_message');
+function tostOn(){
+    tostMessage.classList.add('active');
+    setTimeout(function(){
+        tostMessage.classList.remove('active');
+    },1000);
+}
+
+/*
 1. 입금을 하면 잔액에 + 
 2. 거스름돈 반환을 하면 잔액 -> 0
 */
@@ -9,8 +21,6 @@ let changeForm = document.getElementById('change');
 let insertCoin = 0
 let change = 0;
 
-let tost = document.getElementById('tost_message');
-
 insertCoinBtn.addEventListener('click',function(){
     if(document.getElementById('input_m').value == 0){
         alert('입금하실 금액을 입력해 주세요!');
@@ -18,8 +28,9 @@ insertCoinBtn.addEventListener('click',function(){
         let insertCoin = document.getElementById('input_m').value;
         change = Number(insertCoin) + Number(change);
         changeForm.innerHTML = change.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '<span class="unit">원</span>';
-        tost.innerHTML = insertCoin + '원이 입금 되었습니다!'
+        tostMessage.innerHTML = insertCoin + '원이 입금 되었습니다!'
         document.getElementById('input_m').value = null;
+        tostOn();
     }
 
 });
@@ -27,3 +38,4 @@ changeBtn.addEventListener('click',function(){
     change = 0;
     changeForm.innerHTML = change + '<span class="unit">원</span>';
 });
+
