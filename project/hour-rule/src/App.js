d1be58ee5f00cfdaca04c69349e175d065d0fb15
seungmi <seungmimi10@gmail.com>
2023-09-19 17:45:46 +0900

    💄[update folder]hour-rule

--- project/hour-rule/src/App.js
+++ project/hour-rule/src/App.js
@@ -4,15 +4,19 @@ import Title from './Component/Title';
 import Main from './Component/Main';
 import Footer from './Component/Footer';
 import Modal from './Component/Modal';
+import { useState } from 'react';
 
 function App() {
+
+  const [modalShow, setModalShow] = useState(false);
+
   return (
     <div className={styles.container}>
         <div className={styles.contentArea}>
           <Title />
-          <Main />
+          <Main setModalShow = {setModalShow}/>
           <Footer />
-          {/* <Modal /> */}
+          {modalShow ? <Modal setModalShow = {setModalShow}/> : null};
         </div>
     </div>
   );
--- project/hour-rule/src/Component/Main.jsx
+++ project/hour-rule/src/Component/Main.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styles from './css/Main.module.css';
+import { useRef } from 'react';
 
 //계산 -> 화면 표시 함수
 function OpenResult(props){
@@ -10,55 +11,53 @@ function OpenResult(props){
                 <p>대략 <strong>{props.time}</strong>일 이상 훈련하셔야 합니다! :)</p>
             </div>
             <div>
-                <button className={styles.basicBtn}>훈련하러 가기 GO! GO!</button>
+                <button className={styles.basicBtn} onClick={() => props.setModalShow(true)}>훈련하러 가기 GO! GO!</button>
                 <button className={`${styles.basicBtn} ${styles.white}`}>공유하기</button>
             </div>
         </section>
     )
 }
 
-function Input(){
-    //목표 입력값 저장 함수
+function Main({setModalShow}) {
+    //목표,시간 입력
     const [goal, setGoal] = useState("");
-    const goalInput = (event)=>{
-        setGoal(event.target.value);
-        console.log(goal);
-    }
-    //훈련시간 입력값 저장 함수
+    const goalValue = useRef(null);
+
     const [time, setTime] = useState("");
-    const timeInput = (event)=>{
-        setTime( parseInt(10000 / parseInt(event.target.value)));
-        console.log(time);
+    const timeValue = useRef(null);
+
+    const [result, setInput] = useState(false);
+
+    const submitGaol = (e) => {
+        e.preventDefault();
+        if(goalValue.current.value === ""){
+            alert('어떤전문가가 되고싶은지 입력해 주세요!');
+            goalValue.current.focus();
+            return;
+        }else if(timeValue.current.value === ""){
+            alert('하루 훈련시간을 입력해 주세요!');
+            timeValue.current.focus();
+            return;
+        }
+        setInput(true);
+        setGoal(goalValue.current.value);
+        setTime( parseInt(10000 / parseInt(timeValue.current.value)));
+        console.log(goal, time);
     }
 
     //결과 화면 UI 표시 함수
-    const [result, setInput] = useState(false);
-    function showResult(event){
-        event.preventDefault();
-        setInput(true);
-        console.log(result);
-        console.log(goal,time);
-    }
-    return(
-        <>
-            <section className={styles.insertDataWrap}>
-                <div className={styles.insertData}>
-                    <p>나는 <input type='text' placeholder='예)프로그래밍' className={styles.basicInput} onChange={goalInput}></input> 전문가가 될 것이다.</p>
-
-                    <p>그래서 앞으로 매일 하루에 <input type='number' placeholder='예)5시간' className={styles.basicInput} onChange={timeInput}></input> 시간씩 훈련할 것이다.</p>
-                </div>
-                    <button className={styles.basicBtn} onClick={showResult}type='submit'>나는 며칠 동안 훈련을 해야 1만 시간이 될까?</button>
-            </section>
-            {result ? <OpenResult goal={goal} time={time}/> : null}
-        </>
-
-    );
-}
-
-function Main() {
     return (
     <main>
-        <Input />
+        <section className={styles.insertDataWrap}>
+            <form onSubmit={submitGaol}>
+                <div className={styles.insertData}>
+                    <p>나는 <input type='text' placeholder='예)프로그래밍' className={styles.basicInput}  ref={goalValue}></input> 전문가가 될 것이다.</p>
+                    <p>그래서 앞으로 매일 하루에 <input type='number' placeholder='예)5시간' className={styles.basicInput} ref={timeValue}></input> 시간씩 훈련할 것이다.</p>
+                </div>
+                <button className={styles.basicBtn} type='submit'>나는 며칠 동안 훈련을 해야 1만 시간이 될까?</button>
+            </form>
+        </section>
+        {result ? <OpenResult goal={goal} time={time} setModalShow ={setModalShow}/> : null}
     </main>
     );
 }
