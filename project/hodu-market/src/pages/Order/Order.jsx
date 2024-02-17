cc98aa6316a6055dfbcdcaf157f9c84add474ccd
seungmimi <seungmimi10@gmail.com>
2024-02-17 21:17:21 +0900

    🚀[update folder]주문/결제 UI 작업 완료

--- project/hodu-market/src/pages/Order/Order.jsx
+++ project/hodu-market/src/pages/Order/Order.jsx
@@ -86,7 +86,7 @@ function Order() {
         </form>
 
         <div className={styles['pay-info']}>
-          <div className='payment-info'>
+          <div className={styles['payment-info']}>
             <div className={styles['order-info-title']}>
               <h3>결제 수단</h3>
             </div>
@@ -103,19 +103,39 @@ function Order() {
                 <input type='radio' name='payment' />
                 휴대폰 결제
               </label>
-              <label>
-                <input type='radio' name='payment' />
-                네이버 페이
-              </label>
-              <label>
-                <input type='radio' name='payment' />
-                카카오 페이
-              </label>
             </fieldset>
           </div>
           <div className={styles['sumprice-info']}>
             <div className={styles['order-info-title']}>
               <h3>최종결제 정보</h3>
+            </div>
+            <div className={styles['sumprice-box']}>
+              <div className={styles['sum-obj-box']}>
+                <div className={styles['sum-obj']}>
+                  <span>- 상품금액</span>
+                  <p><strong>46000</strong>원</p>
+                </div>
+                <div className={styles['sum-obj']}>
+                  <span>- 할인금액</span>
+                  <p><strong>0</strong>원</p>
+                </div>
+                <div className={styles['sum-obj']}>
+                  <span>- 배송비</span>
+                  <p><strong>1000</strong>원</p>
+                </div>
+                <div className={`${styles['sum-obj']} ${styles['last-obj']}`}>
+                  <span>- 결제금액</span>
+                  <p><strong>1000</strong>원</p>
+                </div>
+              </div>
+              <div className={styles['go-pay']}>
+                <label className={styles['agree-check']}>
+                  <input type='checkbox' className='hodu-check' />
+                  <span className='hodu-check-img'></span>
+                  <p className={styles['info-text']}>주문 내용을 확인하였으며, 정보 제공 등에 동의합니다.</p>
+                </label>
+                <BasicBtn>결제하기</BasicBtn>
+              </div>
             </div>
           </div>
         </div>
